0219cdabc9f44105ed61d7f1110a270624a80080
Monikvb <monikavb93@gmail.com>
2024-05-19 18:48:06 +0200

    Nuevo code del 8 al 18

--- Evaluacion_mod2_Monica.sql
+++ Evaluacion_mod2_Monica.sql
@@ -53,33 +53,100 @@ un actor_id entre 10 y 20.*/
 /* 9. Encuentra la cantidad total de películas en cada clasificación 
 de la tabla film y muestra la clasificación junto con el recuento.*/
 	
-    SELECT title AS 'Name_Movies', COUNT(rating)
+    SELECT DISTINCT rating, COUNT(*) AS 'Total_Movies'
     FROM film
-	GROUP BY rating
+    GROUP BY rating;
     
 /* 10. Encuentra la cantidad total de películas alquiladas por cada cliente
  y muestra el ID del cliente, su nombre y apellido junto con la cantidad de películas alquiladas.*/
  
+	SELECT customer.customer_id, 
+			customer.first_name, 
+			customer.last_name, 
+			COUNT(rental.rental_id) AS 'Rental_Number'
+    FROM customer
+    LEFT JOIN rental
+		ON customer.customer_id = rental.customer_id
+	GROUP BY customer.customer_id,
+			customer.first_name, 
+			customer.last_name;
+    
 /* 11. Encuentra la cantidad total de películas alquiladas por categoría 
 y muestra el nombre de la categoría junto con el recuento de alquileres.*/
+
+-- Como desarrollarlo:
+-- cantidad peliculas alquiladas con variable:rental_id
+-- total categoria : category_id
+-- nombre categoria : name
+-- conectar las tablas por el id en común
+
+	SELECT category.category_id,
+			category.name,
+			COUNT(rental.rental_id) AS 'Rental_Number'
+    FROM category
+		INNER JOIN film_category ON category.category_id = film_category.category_id
+        INNER JOIN film ON film_category.film_id = film.film_id
+        INNER JOIN inventory ON inventory.film_id = film.film_id
+		INNER JOIN rental ON rental.inventory_id = inventory.inventory_id
+	GROUP BY category.category_id, category.name;
 
 /* 12. Encuentra el promedio de duración de las películas para cada clasificación 
 de la tabla film y muestra la clasificación junto con el promedio de duración.*/
 
+	SELECT rating, AVG(rental_duration) AS 'Average_Length_Film'
+    FROM film
+    GROUP BY rating;
+
 /* 13. Encuentra el nombre y apellido de los actores que aparecen en la película 
 con title "Indian Love".*/
 
-/*1 4. Muestra el título de todas las películas que contengan la palabra 
+SELECT film.title,
+		actor.first_name, 
+		actor.last_name
+FROM actor
+	INNER JOIN film_actor ON actor.actor_id = film_actor.actor_id
+	INNER JOIN film ON film_actor.film_id = film.film_id
+WHERE film.title = 'Indian Love';
+
+/*14. Muestra el título de todas las películas que contengan la palabra 
 "dog" o "cat" en su descripción.*/
 
-/* 15. Hay algún actor o actriz que no apareca en ninguna película en la tabla film_actor.*/
+	SELECT title AS 'Name_Movies', description
+	FROM film
+	WHERE description REGEXP "dog" OR "cat";
 
+/* 15. Hay algún actor o actriz que no aparezca en ninguna película en la tabla film_actor.*/
+	
+    SELECT actor.actor_id, actor.first_name, actor.last_name
+    FROM actor
+	LEFT JOIN film_actor ON actor.actor_id = film_actor.actor_id;
+    
 /* 16. Encuentra el título de todas las películas que fueron lanzadas entre el año 2005 y 2010.*/
+	
+    SELECT title AS 'Name_Movies', release_year
+    FROM film
+    WHERE release_year BETWEEN 2005 AND 2010;
 
 /* 17. Encuentra el título de todas las películas que son de la misma categoría que "Family".*/
 
+	SELECT film.title AS 'Name_Movie', category.name AS 'Renge_Movies'
+	FROM film
+		INNER JOIN film_category ON film.film_id = film_category.film_id
+		INNER JOIN category ON film_category.category_id = category.category_id
+	WHERE category.name = 'Family';
+
 /* 18. Muestra el nombre y apellido de los actores que aparecen en más de 10 películas.*/
 
+	SELECT actor.first_name, 
+			actor.last_name,
+            COUNT(film_actor.film_id) AS 'Total_Movies'
+    FROM actor
+	INNER JOIN film_actor ON actor.actor_id = film_actor.actor_id
+    GROUP BY actor.actor_id,
+			actor.first_name, 
+			actor.last_name
+    HAVING COUNT(film_actor.film_id) > 10;
+    
 /* 19. Encuentra el título de todas las películas que son "R" y tienen una 
 duración mayor a 2 horas en la tabla film.*/
 
